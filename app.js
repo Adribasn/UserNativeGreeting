742c17d63d01508b0838d5308254f15977741ee8
adribasn <adrien.basyn@outlook.com>
2021-05-24 15:55:58 +0200

    finished project

--- app.js
+++ app.js
@@ -14,13 +14,38 @@ $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
     })
     .then(function(jsondata) {
         submit.addEventListener('click', function() { 
-            greeting.style.opacity = 0;
-            greeting.innerHTML = '<mark class="purple">' + jsondata.hello + '</mark>' + '<pre> </pre>' + '<mark class="purple">' + userName.value + '</mark>' + ", You have succesfully logged in!";
-            window.setTimeout(function() {
-                greeting.style.opacity = 1;
-            }, 1000);
+            if (userName.value.length == 0) {
+                userName.style.border = "1px solid #DD0000";
+                userName.style.color = "#DD0000";
+            } else {
+                userName.style.border = "1px solid #ddd";
+                userName.style.color = "#000";
+            }
+
+            if (userPassword.value.length == 0) {
+                userPassword.style.border = "1px solid #DD0000";
+                userPassword.style.color =  "#DD0000";
+            } else {
+                userPassword.style.border = "1px solid #ddd";
+                userPassword.style.color = "#000";
+            }
+                
+            if (userName.value.length > 0 && userPassword.value.length > 0) {
+                greeting.style.opacity = 0;
+                greeting.innerHTML = '<mark class="purple">' + jsondata.hello + '</mark>' + '<pre> </pre>' + '<mark class="purple">' + userName.value + '</mark>' + ", You have succesfully logged in!";
+                window.setTimeout(function() {
+                    greeting.style.opacity = 1;
+                }, 1000);
+                
+                userName.style.border = "1px solid #ddd";
+                userName.style.color = "#000";
+
+                userPassword.style.border = "1px solid #ddd";
+                userPassword.style.color = "#000";
+            }
         });
     })
 });
+
 
 
